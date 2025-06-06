2de2f610c4fde8dad0dffbdebfa92f90a306f92e
swapnil3101 <shay.dixt@gmail.com>
2025-06-06 18:52:22 +0530

    operators

---
+++ finalpractice/Operators.py
@@ -0,0 +1,45 @@
+# 1 arithmatic operator
+# 2 relational operator
+# 3 logical operator
+
+# 1 - arithmatic operator
+a=5
+b=2
+
+print(a+b)
+
+print(a-b)
+
+print(a/b)
+
+print(a//b)
+
+print(a%b)
+
+print(a*b)
+
+print(a**b)
+
+# Relational operators = < > <= >= != // it always returns boolean value
+
+a=5
+b=10
+print(a<b)
+
+print(a>b)
+
+print(a==b)
+
+
+# logical operator = and or not
+
+# and =
+a=True
+b=True
+print(a and b)
+
+#or
+a=True
+b=False
+
+print(a or b)

2318b919e10fc00392f96de9002b3c81b174329e
swapnil3101 <shay.dixt@gmail.com>
2025-06-06 18:42:55 +0530

    initial commit1

---
+++ finalpractice/grocery_store.py
@@ -0,0 +1,5 @@
+print("Welcome to grocery store")
+First_Item = int(input("Enter price of First item:"))
+Second_Item = int(input("Enter price of second item:"))
+total = First_Item + Second_Item
+print("You have to pay:",total)

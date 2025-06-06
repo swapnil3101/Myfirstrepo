c54f2556bb3d3a3adf787561ae75ccfc4dc9bd74
swapnil3101 <shay.dixt@gmail.com>
2025-06-06 19:28:43 +0530

    jumpingstatements_break_continue

---
+++ finalpractice/jumpingstatements_break.py
@@ -0,0 +1,13 @@
+# for i in range(1,20):
+#     if i==5:
+#         continue
+#     print(i)
+for i in range(0,20):
+    if i==5:
+        break
+    print(i)
+
+# for i in range(0,20):
+#     if i==5 or i==9 or i==11:
+#         continue
+#     print(i)

c532c6f3ea8e6f71fcab97ae934ba5de7e65125f
swapnil3101 <shay.dixt@gmail.com>
2025-06-06 18:59:43 +0530

    compare_score_using_if_else

---
+++ finalpractice/compare_score_using_if_else.py
@@ -0,0 +1,9 @@
+score1 = int(input("Enter score of 1st student:"))
+score2 = int(input("enter score of 2nd student:"))
+
+if score1 > score2:
+    print("First student wins")
+elif score1 == score2:
+    print("Its a tie")
+else:
+    print("second student wins")
